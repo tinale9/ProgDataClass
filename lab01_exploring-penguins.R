ff14499596ce5a2c3f9fdbdccf3d2d333e48c6bf
tinale9 <97467748+tinale9@users.noreply.github.com>
2022-02-01 11:44:29 -0500

    Lab 1

---
+++ lab01_exploring-penguins.R
@@ -0,0 +1,25 @@
+# Install packages
+install.packages("tidyverse")
+install.packages("palmerpenguins")
+
+#Load dataset
+library(palmerpenguins)
+library(tibble)
+data <- palmerpenguins::penguins
+
+#Display first 10 rows
+head(data,10)
+
+#Display last 6 rows
+tail(data,10)
+
+#Number of rows in data
+nrow(data)
+
+#Show row Names
+row.names(data)
+
+#Show column names
+names(data)
+
+
